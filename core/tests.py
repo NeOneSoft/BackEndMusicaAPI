84f6cb778d2779d06f665c7b3a1d9a3281160ab3
Gonzalo Romero <gonclapton@hotmail.com>
2019-11-27 21:26:32 -0600

    'commit'

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TokenTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='admin', password='admin', email='admin@gmail.com')
+
+    def test_authenticate(self):
+        result = self.client.post('/api/token/', {'username': 'admin', 'password': 'admin'})
+
+        assert 'access' in result.data
+
+    def test_valid_token(self):
+        result = self.client.post('/api/token/', {'username': 'admin', 'password': 'admin'})
+        token = result.data['access']
+
+        canciones_result = self.client.get('/api/v1/canciones/', HTTP_AUTHORIZATION='Bearer ' + token)
+        artistas_result = self.client.get('/api/v1/artistas/', HTTP_AUTHORIZATION='Bearer ' + token)
+        albumes_result = self.client.get('/api/v1/albumes/', HTTP_AUTHORIZATION='Bearer ' + token)
+
+        assert canciones_result.status_code, artistas_result == 200
+        assert albumes_result.status_code == 200
+
+    def test_valid_token(self):
+        result = self.client.post('/api/token/', {'username': 'admin', 'password': 'admin'})
+        token = result.data['access']
+
+        canciones_result = self.client.get('/api/v1/canciones/', HTTP_AUTHORIZATION='Bearer ' + token)
+        artistas_result = self.client.get('/api/v1/artistas/', HTTP_AUTHORIZATION='Bearer ' + token)
+        albumes_result = self.client.get('/api/v1/albumes/', HTTP_AUTHORIZATION='Bearer ' + token)
+
+        assert canciones_result, artistas_result.status_code == 200
+        assert albumes_result.status_code == 200
+
+
+
+
+
--- musica/urls.py
+++ musica/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include('core.urls.v1')),
     path('admin/', admin.site.urls),
     path('docs/', include_docs_urls(title='Musica API', public=True))
